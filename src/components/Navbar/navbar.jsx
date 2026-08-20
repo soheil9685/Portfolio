339af0fc88849263336f8b2a158b013243d8afc5
SoheilHosseini <canopus.soheilhosseini@gmail.com>
2026-08-21 00:52:11 +0330

    Debug Navbar

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -28,10 +28,10 @@ function Navbar() {
                         </svg>
                     </button>
                 </div>
-                <a className="inter my-2.5 mx-3 lg:mx-7 text-lg text-[#A7ADA5] hidden md:inline hover:text-[#9FE870] hover:text-shadow-[0_0_15px] hover:text-shadow-[#9FE870] transition-all duration-300" href="#">Home</a>
-                <a className="inter my-2.5 mx-3 lg:mx-7 text-lg text-[#A7ADA5] hidden md:inline hover:text-[#9FE870] hover:text-shadow-[0_0_15px] hover:text-shadow-[#9FE870] transition-all duration-300" href="#">Skills</a>
-                <a className="inter my-2.5 mx-3 lg:mx-7 text-lg text-[#A7ADA5] hidden md:inline hover:text-[#9FE870] hover:text-shadow-[0_0_15px] hover:text-shadow-[#9FE870] transition-all duration-300" href="#">Projects</a>
-                <a className="inter my-2.5 mx-3 lg:mx-7 text-lg text-[#A7ADA5] hidden md:inline hover:text-[#9FE870] hover:text-shadow-[0_0_15px] hover:text-shadow-[#9FE870] transition-all duration-300" href="#">About</a>
+                <a className="inter my-2.5 mx-3 lg:mx-7 text-lg text-[#A7ADA5] md:inline hover:text-[#9FE870] hover:text-shadow-[0_0_15px] hover:text-shadow-[#9FE870] transition-all duration-300" href="#">Home</a>
+                <a className="inter my-2.5 mx-3 lg:mx-7 text-lg text-[#A7ADA5] md:inline hover:text-[#9FE870] hover:text-shadow-[0_0_15px] hover:text-shadow-[#9FE870] transition-all duration-300" href="#">Skills</a>
+                <a className="inter my-2.5 mx-3 lg:mx-7 text-lg text-[#A7ADA5] md:inline hover:text-[#9FE870] hover:text-shadow-[0_0_15px] hover:text-shadow-[#9FE870] transition-all duration-300" href="#">Projects</a>
+                <a className="inter my-2.5 mx-3 lg:mx-7 text-lg text-[#A7ADA5] md:inline hover:text-[#9FE870] hover:text-shadow-[0_0_15px] hover:text-shadow-[#9FE870] transition-all duration-300" href="#">About</a>
             </div>
             <div>
                 <button className="border p-2 rounded-md text-[#A7ADA5] border-[#273029] mr-10 lg:mr-0 hover:text-[#9FE870] hover:border-[#9FE870] hover:shadow-[0_0_15px] hover:shadow-[#9FE870] transition-all duration-300">
@@ -39,7 +39,7 @@ function Navbar() {
                         <path strokeLinecap="round" strokeLinejoin="round" d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" />
                     </svg>
                 </button>
-                <button onClick={sideBarStatus} className="text-[#A7ADA5] border-[#273029] inline lg:hidden hover:text-[#9FE870] hover:border-[#9FE870] hover:shadow-[0_0_15px] hover:shadow-[#9FE870] transition-all duration-300">
+                <button onClick={sideBarStatus} className="text-[#A7ADA5] border-[#273029] inline lg:hidden hover:text-[#9FE870] hover:border-[#9FE870] transition-all duration-300">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                     </svg>
