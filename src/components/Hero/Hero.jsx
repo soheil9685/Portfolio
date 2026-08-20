c191aa9a6305b544f4565fd63d85c5347d24ec8f
SoheilHosseini <canopus.soheilhosseini@gmail.com>
2026-08-21 02:10:00 +0330

    Debug Hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -11,16 +11,16 @@ function Hero() {
         <section className="mt-15 px-25 grid grid-cols-12 border-b-2 border-[#273029]">
             <div className="col-span-12 mt-12 text-center mb-10 lg:col-span-6 lg:text-start">
                 <h5 className="text-[#9FE870] inter text-2xl my-2">HI, I'M</h5>
-                <h1 className="text-7xl my-2 text-[#F4F1E8] text-shadow-[0_0_5px] text-shadow-[#F4F1E8]">Mohammad Soheil.</h1>
-                <p className="text-[#9FE870] text-3xl my-3">
+                <h1 className="space-grotesk text-7xl my-2 text-[#F4F1E8] text-shadow-[0_0_5px] text-shadow-[#F4F1E8]">Mohammad Soheil.</h1>
+                <p className="text-[#9FE870] inter text-3xl my-3">
                     <Typewriter words={["Full Stack Developer"]} cursor={true} />
                 </p>
-                <p className="lg:w-80 my-6 text-md text-justify text-[#F4F1E8]">
+                <p className="lg:w-86 my-6 text-md text-justify text-[#F4F1E8] inter">
                     <Typewriter typeSpeed={20} words={["I build modern, interactive and user-focused web experiences with clean code and great performance."]} cursor={true} />
                 </p>
                 <div className="mt-15">
-                    <button className="cursor-pointer hover:shadow-[0_0_7px] hover:shadow-[#9FE870] mr-4 py-2 px-5 bg-[#9FE870] rounded-lg text-[#0B0D0C] transition-all duration-300">View my works</button>
-                    <button className="cursor-pointer text-[#F4F1E8] py-2 px-5 border-[1.5px] border-[#9FE870] rounded-lg hover:bg-[#9FE870] hover:text-[#0B0D0C] transition-all duration-300 hover:shadow-[0_0_5px] hover:shadow-[#9FE870]">Contact me</button>
+                    <button className="inter cursor-pointer hover:shadow-[0_0_7px] hover:shadow-[#9FE870] mr-4 py-2 px-5 bg-[#9FE870] rounded-lg text-[#0B0D0C] transition-all duration-300">View my works</button>
+                    <button className="inter cursor-pointer text-[#F4F1E8] py-2 px-5 border-[1.5px] border-[#9FE870] rounded-lg hover:bg-[#9FE870] hover:text-[#0B0D0C] transition-all duration-300 hover:shadow-[0_0_5px] hover:shadow-[#9FE870]">Contact me</button>
                 </div>
                 <div className="mt-6">
                     <a href="https://github.com/soheil9685">
