288c1699c362a6d3df1931e4114220cfbf9ca6a7
SoheilHosseini <canopus.soheilhosseini@gmail.com>
2026-08-21 23:04:24 +0330

    Debug Footer responsive

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -33,25 +33,25 @@ function Footer() {
 
     return (
         <footer className='bg-[#111613]'>
-            <div className='grid grid-cols-12 px-20 py-10'>
-                <div className='col-span-12 md:col-span-6 lg:col-span-6 xl:col-span-3 mb-10 xl:mb-0 flex justify-center'>
+            <div className='py-18 grid grid-cols-12 mx-auto w-full sm:w-135 md:w-180 lg:w-240 xl:w-285 2xl:w-330'>
+                <div className='col-span-12 md:col-span-6 xl:col-span-3 mb-10 xl:mb-0 m-auto md:mx-0'>
                     <h1 className='inter text-4xl w-55 leading-12 text-justify text-[#F4F1E8]'>
                         LET'S BUILD SOMETHING <span className='text-[#9FE870]'>TOGETHER.</span>
                     </h1> 
                 </div>
-                <div className='col-span-12 md:col-span-6 lg:col-span-6 xl:col-span-4 mb-10 xl:mb-0 flex flex-col items-center justify-center'>
+                <div className='col-span-12 md:col-span-6 xl:col-span-4 mb-10 xl:mb-0 flex flex-col items-start justify-center m-auto md:mx-0'>
                     <p className='text-[#A7ADA5]'>
                         Have a project in mind or just wanna say Hi? <br />
                         I'd love to hear from you.
                     </p>
-                    <button className='bg-[#9FE870] text-[#111613] px-4 py-2 flex justify-center items-center rounded-md inter mt-7 hover:shadow-[0_0_10px] hover:shadow-[#9FE870] transition-all duration-300'>
+                    <button className='m-auto md:mx-0 bg-[#9FE870] text-[#111613] px-4 py-2 flex justify-center items-center rounded-md inter mt-7 hover:shadow-[0_0_10px] hover:shadow-[#9FE870] transition-all duration-300'>
                         <a href="">
                             GET IN TOUCH <ArrowForwardIcon />
                         </a>
                     </button>
                 </div>
                 <div className='divider divider-horizontal divider-success'></div>
-                <div className='col-span-12 md:col-span-6 lg:col-span-12 xl:col-span-4 flex flex-col items-center justify-center'>
+                <div className='col-span-12 lg:col-span-12 xl:col-span-4 m-auto md:mx-0'>
                     {links.map((link) => {
                         const Icon = link.icon
 
@@ -61,7 +61,6 @@ function Footer() {
                             </p>
                         )
                     })}
-
                 </div>
             </div>
             <div className='bg-[#0B0D0C] grid grid-cols-2 py-2'>
