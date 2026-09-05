48a358ebe83547f2101ba013865fed3ce37eb99b
SoheilHosseini <canopus.soheilhosseini@gmail.com>
2026-09-05 09:48:33 +0330

    debug

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -32,7 +32,7 @@ function Hero({ darkMode }) {
 
     return (
         <section id="hero" className="pt-15 border-b-2 border-[#E5E8E3] dark:border-[#273029] bg-[#F7F8F6] dark:bg-[#0B0D0C]">
-            <div className="mx-auto w-87.5 sm:w-135 md:w-180 lg:w-240 xl:w-285 2xl:w-330 grid grid-cols-12">
+            <div className="mx-auto w-full sm:w-135 md:w-180 lg:w-240 xl:w-285 2xl:w-330 grid grid-cols-12">
                 <div className="col-span-12 mt-12 text-center mb-10 lg:col-span-6 lg:text-start">
                     <h5 className="text-[#667085] dark:text-[#9FE870] inter text-2xl my-2">HI, I'M</h5>
                     <h1 className="space-grotesk text-7xl my-2 text-[#171B20] dark:text-[#F4F1E8]">Mohammad Soheil.</h1>
